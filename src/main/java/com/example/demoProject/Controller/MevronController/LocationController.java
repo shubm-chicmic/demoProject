984e3744c522ab57fee865622ca07096fee2db12
shubm-chicmic <shubham.mishra@chicmic.co.in>
2023-05-12 16:15:12 +0530

    mevron-edit

--- src/main/java/com/example/demoProject/Controller/MevronController/LocationController.java
+++ src/main/java/com/example/demoProject/Controller/MevronController/LocationController.java
@@ -1,15 +1,24 @@
 package com.example.demoProject.Controller.MevronController;
 
+import com.example.demoProject.Models.Vehicle;
+import com.example.demoProject.Service.VehicleService;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LocationController {
-    @PostMapping("/pickDrop")
-    public String pickDrop(HttpServletRequest request) {
+    @Autowired
+    VehicleService vehicleService;
+    @GetMapping("/pickDrop")
+    public String pickDrop(HttpServletRequest request, Model model) {
         long vehicleId = Long.parseLong(request.getParameter("id"));
+        Vehicle vehicle = vehicleService.findVehicleById(vehicleId);
+        model.addAttribute("vehicle", vehicle);
         System.out.println("\u001B[42m" + "vehicleid = " + vehicleId + "\u001B[0m");
         return "pick-drop";
     }
